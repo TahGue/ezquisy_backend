5a675770445bed6cf6aac13654f9e850c5056056
1453 <t.guemir@gmail.com>
2021-11-01 15:17:59 +0100

    auth rout

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-
+const auth = require('./src/routes/auth');
 const user = require('./src/routes/user');
 const questionType = require('./src/routes/questionType');
 const category = require('./src/routes/category');
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use ('/auth', auth)
 app.use('/question', question);
 app.use('/category', category); 
 
